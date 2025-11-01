7509d3e5c901963c32674115bf8b10ed0975098d
DrVuLongTuyen <vltuyen@vltuyen.com>
2025-11-01 22:40:56 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,7 @@ from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from datetime import datetime 
 import traceback 
+from datetime import datetime
 
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
@@ -325,3 +326,4 @@ def handle_mix():
 # Chạy ứng dụng (cho Render)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
