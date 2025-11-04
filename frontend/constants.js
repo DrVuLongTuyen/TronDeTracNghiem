159d9e04eff496be8e6a82f18d5439968adb8ac0
DrVuLongTuyen <vltuyen@vltuyen.com>
2025-11-04 22:33:35 +0700

    Create constants.js

---
+++ frontend/constants.js
@@ -0,0 +1,9 @@
+// ==================================================================
+// == QUAN TRỌNG: HÃY ĐẢM BẢO 3 DÒNG NÀY LÀ CỦA BẠN ==
+// ==================================================================
+export const SUPABASE_URL = 'https://iezcijerbmsgsxilsixo.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllemNpamVyYm1zZ3N4aWxzaXhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NDg5MjAsImV4cCI6MjA3NzEyNDkyMH0.g_VIc4KIpJhKutJ3rBCbKB02gKcjhmaGMsvbG9rjcUk';
+export const API_BASE_URL = 'https://trondetn-api.onrender.com';
+// ==================================================================
+
+export const { createClient } = window.supabase;
